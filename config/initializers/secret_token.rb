dbbb53bbb08200120dd04a4ad851fa9ba3609ab5
Alyssa Levitz <alyssal@yammer-inc.com>
2014-04-17 20:34:02 -0700

    basic app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chooselunch::Application.config.secret_key_base = 'edd30f084a7bfbd1b98a18c47573f94fb5fee1db506b41c8a54621d3d37943aa0378cede7fc99b73e4d3708bfc4ca119da6978606d93e6721504df3f8e782fd3'
